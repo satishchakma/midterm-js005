17d218ce1e05390e006cfc47e90bdde76b581d59
Satish Chakma <77580640+satishchakma@users.noreply.github.com>
2021-01-23 15:21:30 +0600

    Add files via upload

---
+++ midterm-05.js
@@ -0,0 +1,47 @@
+let itisraining = true;
+let gotooffice = true;
+let ihavebeef = false;
+
+if (itisraining == true) {
+    if (gotooffice == true){
+        console.log("take umbrella");
+    }
+    else if(gotooffice == false && ihavebeef == true) {
+        console.log("send mail");
+        console.log("cook khichuri");
+        console.log("cook beef and take with khichuri ");
+    }
+    else {
+        console.log("omlette egg and take with khichuri");
+    }
+    
+
+}
+
+else{
+    console.log("no output");
+}
+
+// let itisraining = true;
+// let gotooffice = false;
+// let ihavebeef = false;
+
+// if (itisraining == true) {
+//     if (gotooffice == true){
+//         console.log("take umbrella");
+//     }
+//     else if(gotooffice == false && ihavebeef == true) {
+//         console.log("send mail");
+//         console.log("cook khichuri");
+//         console.log("cook beef and take with khichuri ");
+//     }
+//     else {
+//         console.log("omlette egg and take with khichuri");
+//     }
+    
+
+// }
+
+// else{
+//     console.log("no output");
+// }
